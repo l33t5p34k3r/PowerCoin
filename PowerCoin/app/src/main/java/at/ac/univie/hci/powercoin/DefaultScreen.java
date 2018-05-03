1a1665245c04c93f90497702802296bb3c1ff279
l33t5p34k3r <konstantin.petz@outlook.at>
2018-05-03 16:12:05 +0200

    implemented 'graph view' and added example graph

--- PowerCoin/app/src/main/java/at/ac/univie/hci/powercoin/DefaultScreen.java
+++ PowerCoin/app/src/main/java/at/ac/univie/hci/powercoin/DefaultScreen.java
@@ -9,6 +9,10 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class DefaultScreen extends AppCompatActivity {
 
     @Override
@@ -26,6 +30,16 @@ public class DefaultScreen extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 
     @Override
