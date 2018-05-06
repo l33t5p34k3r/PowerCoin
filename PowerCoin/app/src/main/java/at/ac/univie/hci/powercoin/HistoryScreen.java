231c19efe32cf2c4d7dacdee9d92cfc365310304
Razvan-Nicolae Nancu <a1502339@unet.univie.ac.at>
2018-05-06 15:19:42 +0200

    Added History screen Get Bitcoin from file done Portfolio main functions done
    TODO: Show current bitcoin on Portfolio Screen and calculate worth in Euro/Dollar

---
+++ PowerCoin/app/src/main/java/at/ac/univie/hci/powercoin/HistoryScreen.java
@@ -0,0 +1,39 @@
+package at.ac.univie.hci.powercoin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.File;
+
+public class HistoryScreen  extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView historyView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.portfolio_history_screen);
+
+
+
+        Intent intent = getIntent();
+
+        historyView = findViewById(R.id.textViewHist);
+
+        String historyText = intent.getStringExtra(PortfolioScreen.HISTORY_MESSAGE);
+
+        historyView.setText(historyText);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+
+}
--- PowerCoin/app/src/main/java/at/ac/univie/hci/powercoin/PortfolioScreen.java
+++ PowerCoin/app/src/main/java/at/ac/univie/hci/powercoin/PortfolioScreen.java
@@ -19,24 +19,36 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PortfolioScreen extends AppCompatActivity implements View.OnClickListener {
 
     private TextInputLayout usernameWrapper;
     double bitcoinAmmount;
+    public static final String HISTORY_MESSAGE = "historyFile";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.portfolio_default_screen);
 
-        writeToFile("Test Data 2", this);
+        //writeToFile("Test Data 2", this);
 
-        Log.d("FILE", readFromFile(this));
-        System.out.println(readFromFile(this));
-        Log.d("FILE", PortfolioScreen.this.getFilesDir().getAbsolutePath());
+        File file = new File(PortfolioScreen.this.getFilesDir().getAbsolutePath(), "PortfolioHistory.txt");
+        if(file.exists()){
+            portfolioStart();
+        }
 
-        this.deleteFile("PortfolioHistory.txt"); //THIS IS USED TO DELETE FILE FOR DEBUG PURPOSES
+
+        // Log.d("FILE", readFromFile(this));
+        // System.out.println(readFromFile(this));
+        // Log.d("FILE", PortfolioScreen.this.getFilesDir().getAbsolutePath());
+
+        //this.deleteFile("PortfolioHistory.txt"); //THIS IS USED TO DELETE FILE FOR DEBUG PURPOSES
 
 
         Button buttonAdd = findViewById(R.id.buttonAdd);
@@ -51,9 +63,28 @@ public class PortfolioScreen extends AppCompatActivity implements View.OnClickLi
         usernameWrapper = (TextInputLayout) findViewById(R.id.textInputBTC);
 
 
+
     }
 
     //History
+
+    //ON START, get the last (most recent) amount of Bitcoin from the wallet
+    public void portfolioStart(){
+        String str = readFromFile(this);
+
+        String[] parts = str.split("[\\)]");
+        String part2 = parts[parts.length-1];
+
+        Pattern p = Pattern.compile("(\\d+(?:\\.\\d+))");
+        Matcher m = p.matcher(part2);
+        double d = 0;
+        while(m.find()) {
+            d = Double.parseDouble(m.group(1));
+            System.out.println(d);
+        }
+        bitcoinAmmount = d;
+        Log.d("FILE", "CURRENT BITCOIN DEBUG: " + bitcoinAmmount);
+    }
 
 
 
@@ -134,7 +165,7 @@ public class PortfolioScreen extends AppCompatActivity implements View.OnClickLi
                 historyClicked();
                 break;
             default:
-                throw new RuntimeException("Unknow button ID");
+                throw new RuntimeException("Unknown button ID");
         }
 
 
@@ -142,7 +173,10 @@ public class PortfolioScreen extends AppCompatActivity implements View.OnClickLi
 
 
     public void historyClicked(){
-
+        Intent result = new Intent(this, HistoryScreen.class);
+        result.putExtra(HISTORY_MESSAGE, readFromFile(this));
+        //result.putExtra(HISTORY_MESSAGE, "Yo.");
+        startActivity(result);
     }
 
     public void addClicked(){
@@ -150,33 +184,40 @@ public class PortfolioScreen extends AppCompatActivity implements View.OnClickLi
         //if(bitcoinAmmount == null) bitcoinAmmount = 0;
         //usernameWrapper.getEditText().getText().toString();
 
+        String date = "";
         Log.d("ADD_BUTTON", "If statement parse String to double STARTS!");
         if( isDouble( usernameWrapper.getEditText().getText().toString())){
 
             Log.d("ADD_BUTTON", "If statement PASSED!");
             bitcoinAmmount += Double.parseDouble(usernameWrapper.getEditText().getText().toString());
+            date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
         }
         else{
             Toast.makeText(PortfolioScreen.this,
                     "Please enter a number!",
                     Toast.LENGTH_LONG).show();
+                    return;
         }
 
-        writeToFile("BTC: " + bitcoinAmmount + "\n", this);
+        writeToFile(date + " (+" + usernameWrapper.getEditText().getText().toString() + ") BTC: " + bitcoinAmmount +  "\n", this);
         Log.d("ADD_BUTTON", "Bitcoin in wallet: " + bitcoinAmmount);
     }
 
     public void removeClicked(){
+
+        String date = "";
         if( isDouble( usernameWrapper.getEditText().getText().toString())){
             Log.d("REMOVE_BUTTON", "If statement PASSED!");
             bitcoinAmmount -= Double.parseDouble(usernameWrapper.getEditText().getText().toString());
+            date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
         }
         else{
             Toast.makeText(PortfolioScreen.this,
                     "Please enter a number!",
                     Toast.LENGTH_LONG).show();
+            return;
         }
-        writeToFile("BTC: " + bitcoinAmmount + "\n", this);
+        writeToFile(date + " (-" + usernameWrapper.getEditText().getText().toString() + ") BTC: " + bitcoinAmmount +  "\n", this);
         Log.d("REMOVE_BUTTON", "Bitcoin in wallet: " + bitcoinAmmount);
     }
 
